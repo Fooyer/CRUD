a5fb30fa0902400e148d6f72dbc070d5b54e2e92
Fooyer <95866940+Fooyer@users.noreply.github.com>
2022-05-02 21:02:18 -0300

    Add files via upload

---
+++ CRUE (no comments)/conexao/db.php
@@ -0,0 +1,17 @@
+<?php
+
+class DB {
+    protected $dbh;
+
+    function __construct() {
+        try {
+            
+            $this->dbh = new PDO('mysql:host=localhost;dbname=basecadastro', 'root', '');
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+}
