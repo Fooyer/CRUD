d1ed391023df3d638be7124f59ed6c76c47985bc
Freddy Baier <xneerd157@gmail.com>
2022-12-13 14:17:21 -0300

    teste

--- conexao/db.php
+++ conexao/db.php
@@ -6,7 +6,7 @@ class DB {
     function __construct() {
         try {
             
-            $this->dbh = new PDO('mysql:host=${{ secrets.DB_HOST }};dbname=${{ secrets.DB_NAMES }}', '${{ secrets.DB_USERNAME }}', '${{ secrets.DB_PASSWORD }}');
+            $this->dbh = new PDO('mysql:'host=${{ secrets.DB_HOST }}';dbname='${{ secrets.DB_NAMES }}, ${{ secrets.DB_USERNAME }}, ${{ secrets.DB_PASSWORD }});
             $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
